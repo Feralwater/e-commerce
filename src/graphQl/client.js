e070ea2d687c834718af8d84e99de9ae12331dcf
User <2176589@mail.ru>
2021-07-17 19:40:28 -0700

    Add env

--- src/graphQl/client.js
+++ src/graphQl/client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
+  uri: process.env.REACT_APP_GRAPHQL_HOST || 'http://localhost:4000/',
   cache: new InMemoryCache(),
 });
 

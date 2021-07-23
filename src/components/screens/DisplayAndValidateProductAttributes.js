dd3360d62e81e946bea6000bba924e639ca99962
User <2176589@mail.ru>
2021-07-23 12:27:52 -0700

    Fix: DisplayAndValidateProductAttributes - separate methods

--- src/components/screens/DisplayAndValidateProductAttributes.js
+++ src/components/screens/DisplayAndValidateProductAttributes.js
@@ -7,6 +7,58 @@ import {
 } from '../../styleComponents/ProductScreenStyles';
 
 class DisplayAndValidateProductAttributes extends React.PureComponent {
+  getAttributesColor = (attribute, product, validate, startValidate, attributes, setSelectAttribute, setValidate) => (
+    <div key={Math.random() * 100_000}>
+      <Attributes>
+        {attribute.name}
+        :
+      </Attributes>
+      <AttributesContainer>
+        {attribute.items.map((x) => (
+          <Span
+            inStock={product.inStock}
+            validate={validate[attribute.name1]}
+            startValidate={startValidate}
+            active={attributes[attribute.name]}
+            color={x.value}
+            key={Math.random() * 100_000}
+            onClick={() => {
+              setSelectAttribute(x.value, attribute.name);
+              setValidate(attribute.name, true);
+            }}
+          />
+        ))}
+      </AttributesContainer>
+    </div>
+  );
+
+  getAttributes = (attribute, product, validate, startValidate, attributes, setSelectAttribute, setValidate) => (
+    <div key={Math.random() * 100_000}>
+      <Attributes>
+        {attribute.name}
+        :
+      </Attributes>
+      <AttributesContainer>
+        {attribute.items.map((x) => (
+          <Span
+            inStock={product.inStock}
+            validate={validate[attribute.name]}
+            startValidate={startValidate}
+            active={attributes[attribute.name]}
+            value={x.value}
+            key={Math.random() * 100_000}
+            onClick={() => {
+              setSelectAttribute(x.value, attribute.name);
+              setValidate(attribute.name, true);
+            }}
+          >
+            {x.value}
+          </Span>
+        ))}
+      </AttributesContainer>
+    </div>
+  );
+
   render() {
     const {
       product,
@@ -19,61 +71,8 @@ class DisplayAndValidateProductAttributes extends React.PureComponent {
     return (
       <Description>
         {product.attributes.map((attribute) => (attribute.type === 'swatch'
-          ? (
-            (
-              <div key={Math.random() * 100_000}>
-                <Attributes>
-                  {attribute.name}
-                  :
-                </Attributes>
-                <AttributesContainer>
-                  {attribute.items.map((x) => (
-                    <Span
-                      inStock={product.inStock}
-                      validate={validate[attribute.name]}
-                      startValidate={startValidate}
-                      active={attributes[attribute.name]}
-                      color={x.value}
-                      key={Math.random() * 100_000}
-                      onClick={() => {
-                        setSelectAttribute(x.value, attribute.name);
-                        setValidate(attribute.name, true);
-                      }}
-                    />
-                  ))}
-                </AttributesContainer>
-              </div>
-            )
-          )
-          : (
-            (
-              <div key={Math.random() * 100_000}>
-                <Attributes>
-                  {attribute.name}
-                  :
-                </Attributes>
-                <AttributesContainer>
-                  {attribute.items.map((x) => (
-                    <Span
-                      inStock={product.inStock}
-                      validate={validate[attribute.name]}
-                      startValidate={startValidate}
-                      active={attributes[attribute.name]}
-                      value={x.value}
-                      key={Math.random() * 100_000}
-                      onClick={() => {
-                        setSelectAttribute(x.value, attribute.name);
-                        setValidate(attribute.name, true);
-                      }}
-                    >
-                      {x.value}
-                    </Span>
-                  ))}
-                </AttributesContainer>
-              </div>
-            )
-          )))}
-
+          ? this.getAttributesColor(attribute, product, validate, startValidate, attributes, setSelectAttribute, setValidate)
+          : this.getAttributes(attribute, product, validate, startValidate, attributes, setSelectAttribute, setValidate)))}
       </Description>
     );
   }

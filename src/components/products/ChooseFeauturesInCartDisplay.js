bcb3ae44424edece393b9d2e42c6957ef72332ae
User <2176589@mail.ru>
2021-07-23 11:57:50 -0700

    Fix: ChooseProductFeaturesDisplay - separate methods

--- src/components/products/ChooseFeauturesInCartDisplay.js
+++ src/components/products/ChooseFeauturesInCartDisplay.js
@@ -4,47 +4,47 @@ import { AttributesContainer } from '../../styleComponents/ProductScreenStyles';
 import { AttributeName, Span } from '../../styleComponents/ProductInCartStyle';
 
 class ChooseProductFeaturesDisplay extends React.PureComponent {
+  getAttributeColor=(attribute, item) => (
+    <div key={Math.random() * 100_000}>
+      <AttributesContainer>
+        {attribute.items.map((x) => (
+          <Span
+            color={x.value}
+            key={Math.random() * 100_000}
+            active={item.params[attribute.name]}
+          />
+        ))}
+      </AttributesContainer>
+    </div>
+  )
+
+  getAttribute=(attribute, item) => (
+    <div key={Math.random() * 100_000}>
+      <AttributeName>
+        {`${attribute.name}:`}
+      </AttributeName>
+      <AttributesContainer>
+        {attribute.items.map((x) => (
+          <Span
+            key={Math.random() * 100_000}
+            value={x.value}
+            active={item.params[attribute.name]}
+          >
+            {x.value}
+          </Span>
+        ))}
+      </AttributesContainer>
+    </div>
+  )
+
   render() {
     const { item } = this.props;
     return (
       <Features>
         {item.attributes.map((attribute) => (
           attribute.type === 'swatch'
-            ? (
-              (
-                <div key={Math.random() * 100_000}>
-                  <AttributesContainer>
-                    {attribute.items.map((x) => (
-                      <Span
-                        color={x.value}
-                        key={Math.random() * 100_000}
-                        active={item.params[attribute.name]}
-                      />
-                    ))}
-                  </AttributesContainer>
-                </div>
-              )
-            )
-            : (
-              (
-                <div key={Math.random() * 100_000}>
-                  <AttributeName>
-                    {`${attribute.name}:`}
-                  </AttributeName>
-                  <AttributesContainer>
-                    {attribute.items.map((x) => (
-                      <Span
-                        key={Math.random() * 100_000}
-                        value={x.value}
-                        active={item.params[attribute.name]}
-                      >
-                        {x.value}
-                      </Span>
-                    ))}
-                  </AttributesContainer>
-                </div>
-              )
-            )))}
+            ? this.getAttributeColor(attribute, item)
+            : this.getAttribute(attribute, item)))}
       </Features>
     );
   }
